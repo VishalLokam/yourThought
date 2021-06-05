f18062cdf963c46d6e400a67423a807c44ade9cd
vishal <vishalvikaslokam@gmail.com>
2021-06-05 22:22:32 +0530

    mongodb connection error

--- index.js
+++ index.js
@@ -15,59 +15,68 @@ app.use(Express.static("public"));
 app.use(Express.urlencoded({ extended: true }));
 
 mongoose
-  .connect(process.env.mongodb+srv://admin-vishal:1234@cluster0.qm1km.mongodb.net/thoughtbook?retryWrites=true&w=majority, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, console.log(`Server started on port ${PORT}`));
-  })
-  .catch((err) => console.error(err));
+    .connect(
+        "mongodb+srv://admin-vishal:1234@cluster0.qm1km.mongodb.net/thoughtbook?retryWrites=true&w=majority",
+        {
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => {
+        app.listen(PORT, console.log(`Server started on port ${PORT}`));
+    })
+    .catch((err) => console.error(err));
 
 app.get("/home", async (_, res) => {
-  const posts = await Post.find();
+    const posts = await Post.find();
 
-  res.render("home", {
-    posts: posts.map(({ id, title, body, postedOn }) => ({
-      id,
-      title: title.length < 100 ? title : title.substr(0, 100).trim() + "...",
-      body: body.length < 500 ? body : body.substr(0, 400).trim() + "...",
-      postedOn: dayjs().to(dayjs(postedOn)),
-    })),
-  });
+    res.render("home", {
+        posts: posts.map(({ id, title, body, postedOn }) => ({
+            id,
+            title:
+                title.length < 100
+                    ? title
+                    : title.substr(0, 100).trim() + "...",
+            body: body.length < 500 ? body : body.substr(0, 400).trim() + "...",
+            postedOn: dayjs().to(dayjs(postedOn)),
+        })),
+    });
 });
 
 app.get("/add", (_, res) => {
-  res.render("add");
+    res.render("add");
 });
 
 app.post("/add", async (req, res) => {
-  const { title, body } = req.body;
+    const { title, body } = req.body;
 
-  try {
-    await Post.create({ title, body });
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await Post.create({ title, body });
+    } catch (error) {
+        console.log(error);
+    }
 
-  res.redirect("/home");
+    res.redirect("/home");
 });
 
 app.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const { title, body, postedOn } = await Post.findById(id);
+    const { id } = req.params;
+    const { title, body, postedOn } = await Post.findById(id);
 
-  res.render("post", {
-    id,
-    post: {
-      id,
-      title: title.length < 100 ? title : title.substr(0, 100).trim() + "...",
-      body: body.length < 500 ? body : body.substr(0, 400).trim() + "...",
-      postedOn: dayjs().to(dayjs(postedOn)),
-    },
-  });
+    res.render("post", {
+        id,
+        post: {
+            id,
+            title:
+                title.length < 100
+                    ? title
+                    : title.substr(0, 100).trim() + "...",
+            body: body.length < 500 ? body : body.substr(0, 400).trim() + "...",
+            postedOn: dayjs().to(dayjs(postedOn)),
+        },
+    });
 });
 
 const { PORT = 3000 } = process.env;
